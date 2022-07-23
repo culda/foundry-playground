dfe56cde0bebf3df301f2a431721854a5c4d686e
Culda <culda@proton.me>
2022-07-23 17:32:45 +0100

    derp

--- src/UniFlash.sol
+++ src/UniFlash.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.8.15;
+pragma solidity =0.7.6;
 
 // import 'v3-periphery/interfaces/ISwapRouter.sol';
-import 'v3-core-interfaces/callback/IUniswapV3FlashCallback.sol';
+import 'v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol';
 import 'v3-periphery/base/PeripheryPayments.sol';
+import 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 
 contract PairFlash is IUniswapV3FlashCallback, PeripheryPayments {
     using LowGasSafeMath for uint256;
